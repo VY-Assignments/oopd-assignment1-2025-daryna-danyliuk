bfe7d8e448ef57514f5f4974152a28b2fc86c791
ddanyliuk18 <ddanyliuk@kse.org.ua>
2025-09-17 12:42:52 +0300

    Started implementation of board

---
+++ Board.cpp
@@ -0,0 +1,23 @@
+﻿#include "Board.h"
+#include <vector>
+#include <iostream>
+
+Board::Board(int w, int h)
+    : height(h), width(w), canvas(h, std::vector<char>(w,' ')){}
+
+void Board::print(){
+    for (int i = 0; i<height; i++ ) {
+        for (int j = 0; j<width; j++ ) {
+            std::cout << canvas[i][j];
+        }
+        std::cout << std::endl;
+    }
+}
+
+void Board::clear() {
+    for (int i = 0; i<height; i++ ) {
+        for (int j = 0; j<width; j++ ) {
+            canvas[i][j] = ' ';
+        }
+    }
+}
---
+++ Board.h
@@ -0,0 +1,18 @@
+﻿#ifndef BOARD_H
+#define BOARD_H
+#include <vector>
+
+class Board {
+private:
+    int height;
+    int width;
+    std::vector<std::vector<char>> canvas;
+public:
+    Board (int h, int w);
+    void clear();
+    void print();
+};
+
+
+
+#endif //BOARD_H
