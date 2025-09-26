e87f0cec6d31e92df0aac576d80c21506574e4b1
ddanyliuk18 <ddanyliuk@kse.org.ua>
2025-09-26 23:33:55 +0300

    Added diamond realisation

---
+++ Diamond.cpp
@@ -0,0 +1,46 @@
+﻿#include "Diamond.h"
+#include "Shape.h"
+#include "Board.h"
+
+Diamond::Diamond(int id, int x, int y, int w, int h, Colour c, FillStat f)
+    :Shape(id, x, y), width(w), height(h), colour(c), fill(f) {}
+
+bool Diamond :: isInDiamond(int x, int y) {
+    int dx = abs(x - xCoord);
+    int dy = abs(y - yCoord);
+    return (dx * 2.0 / width + dy * 2.0 / height <= 1.0);
+}
+void Diamond :: draw(Board& board) {
+    if (width <= 0 || height <= 0) {
+        std::cout << "Width and height must be positive" << std::endl;
+        return;
+    }
+    char symbol = colourToChar(this->colour);
+    int halfWidth = width/2;
+    int halfHeight = height/2;
+    for (int j = yCoord - halfHeight; j <= yCoord + halfHeight;  ++j) {
+        for ( int i = xCoord - halfWidth; i <= xCoord + halfWidth; ++i) {
+            bool isEndPoint = false;
+            if (!isInDiamond(i-1, j) || !isInDiamond(i+1, j) ||
+                !isInDiamond(i, j-1) || !isInDiamond(i, j+1)) {
+                isEndPoint = true;
+                }
+            int dx = abs(i -xCoord);
+            int dy = abs(j - yCoord);
+            if (isInDiamond) {
+                switch (fill) {
+                    case FillStat::FRAME:
+                        if (isEndPoint) {
+                            board.setCell(i, j, symbol);
+                        }
+                        break;
+                    case FillStat::FILLED:
+                        board.setCell(i, j, symbol);
+                        break;
+                }
+            }
+
+        }
+
+    }
+}
---
+++ Diamond.h
@@ -0,0 +1,27 @@
+﻿#ifndef DIAMOND_H
+#define DIAMOND_H
+#include "Shape.h"
+#include "Board.h"
+
+
+
+class Diamond : Shape {
+private:
+    int width;
+    int height;
+    Colour colour;
+    FillStat fill;
+
+public:
+    Diamond (int id, int x, int y, int w, int h, Colour c, FillStat f);
+    void draw(Board& board) override;
+    void edit() override;
+    void move(int newX, int newY) override;
+    void paint(Colour c) override;
+    std::string info() override;
+    bool isInDiamond(int x, int y);
+};
+
+
+
+#endif //DIAMOND_H
